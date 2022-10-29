66be1543197c8a44d0c91e6dc4a5c9529e1bf91a
Volodymyr <vthehang@gmail.com>
2022-10-29 16:48:25 +0200

    added some animations + file size formatting to client

--- client/src/components/disk/fileList/FileList.jsx
+++ client/src/components/disk/fileList/FileList.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import File from "./file/File";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import "./fileList.css";
 
 const FileList = () => {
-    const files = useSelector((state) => state.files.files).map((file) => (
-        <File key={file._id} file={file} />
-    ));
+    const files = useSelector((state) => state.files.files);
 
     return (
         <div className="filelist">
@@ -15,7 +14,18 @@ const FileList = () => {
                 <div className="filelist__date">Date</div>
                 <div className="filelist__size">Size</div>
             </div>
-            {files}
+            <TransitionGroup>
+                {files.map((file) => (
+                    <CSSTransition
+                        key={file._id}
+                        timeout={500}
+                        classNames={"file"}
+                        onExit={false}
+                    >
+                        <File file={file} />
+                    </CSSTransition>
+                ))}
+            </TransitionGroup>
         </div>
     );
 };
--- client/src/components/disk/fileList/file/File.jsx
+++ client/src/components/disk/fileList/file/File.jsx
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { pushToStack, setCurrentDir } from "../../../../reducers/fileReducer";
 import "./file.css";
 import { deleteFile, downloadFile } from "../../../../actions/file";
+import sizeFormat from "../../../../utils/sizeFormat";
 
 const File = ({ file }) => {
     const dispatch = useDispatch();
@@ -36,7 +37,7 @@ const File = ({ file }) => {
             />
             <div className="file__name">{file.name}</div>
             <div className="file__date">{file.date.slice(0, 10)}</div>
-            <div className="file__size">{file.size}</div>
+            <div className="file__size">{sizeFormat(file.size)}</div>
             {file.type !== "dir" && (
                 <button
                     onClick={(e) => downloadClickHandler(e)}
